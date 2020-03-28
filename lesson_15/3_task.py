c2f72b7c5831ece58431bb009bdd0009227b63eb
Vladyslav92 <53137123+Vladyslav92@users.noreply.github.com>
2020-03-28 19:47:32 +0200

    Update 3_task.py

--- lesson_15/3_task.py
+++ lesson_15/3_task.py
@@ -1,23 +1,25 @@
 # Реализовать дескриптор валидации для атрибута email.
 # Ваш дескриптор должен проверять формат email который вы пытаетесь назначить.
+import re
 
 
 class EmailDescriptor:
     def __get__(self, instance, owner):
         print(instance, owner)
+        return owner
 
     def __set__(self, instance, value):
-        base = []
-        for i in value:
-            if i == '@' or i == '.':
-                base.append(i)
-        if len(base) > 2 or len(base) == 0 or len(base) < 2:
-            print(value, '-', False)
-        else:
+        base = "^[_A-Za-z0-9-\\+]+(\\.[A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        if re.match(base, value):
             print(value, '-', True)
+            return value
+        else:
+            print(value, '-', False)
+            # raise Exception('email is invalid')
 
 
 class MyClass:
+    base = EmailDescriptor()
     email = EmailDescriptor()
 
 
@@ -25,4 +27,5 @@ my_class = MyClass()
 my_class.email = "validemail@gmail.com"
 my_class.email = "de@mail@gmail.com."
 my_class.email = "novalidemail"
+my_class.email = "vs/got@gmail.com"
 print(my_class)
